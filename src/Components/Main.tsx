82adaf7977c209b9d8a713d38d25a92f2a5cc3f4
dani <babadany2999@gmail.com>
2024-02-13 11:33:57 +0100

    created post component

--- src/Components/Main.tsx
+++ src/Components/Main.tsx
@@ -2,6 +2,11 @@ import '../Styles/Main.css';
 import Title from './MultipleUses/Title';
 import bellIcon from '../Assets/Images/bell.svg';
 import Story from './MultipleUses/Story';
+import Post from './MultipleUses/Post';
+// Humans
+import humanOne from '../Assets/Images/humans/human1.jpg';
+// Post
+import postOne from '../Assets/Images/posts/postOne.jpg';
 
 const Main = () => {
     return <main className="Main">
@@ -10,6 +15,18 @@ const Main = () => {
             right: true
         }}}/>
         <Story/>
+        <Post ctx={{
+            icon: humanOne,
+            handle: "myhandle",
+            media:  postOne,
+            text: "Look at how mysterous I am! You would never guess I like to pee in the shower :D"
+        }}/>
+        <Post ctx={{
+            icon: humanOne,
+            handle: "myhandle",
+            media:  postOne,
+            text: "Look at how mysterous I am! You would never guess I like to pee in the shower :D"
+        }}/>
     </main>
 }
 
---
+++ src/Components/MultipleUses/Post.tsx
@@ -0,0 +1,41 @@
+import "../../Styles/MultipleUses/Post.css";
+import dotsIcon from '../../Assets/Images/dots.svg';
+import commentIcon from '../../Assets/Images/comment.svg';
+import heartIcon from '../../Assets/Images/heart.svg';
+
+interface IPost {
+    icon: string;
+    handle: string;
+    media?: string;
+    text?: string;
+}
+
+const Post = ({ctx}: {ctx: IPost}) => {
+    return <div className="Post">
+        <div className="Post-Header">
+            <div className="Post-Header-Left">
+                <img src={ctx.icon} alt=""/>
+                <span>{ctx.handle}</span>
+            </div>
+            <button>
+                <img src={dotsIcon} alt=""/>
+            </button>
+        </div>
+        <div className="Post-Main">
+            <img src={ctx.media} alt="/"/>
+            <span>{ctx.text}</span>
+        </div>
+        <div className="Post-Footer">
+            <div className="Post-Footer-Comments">
+                <img src={commentIcon} alt=""/>
+                <span>{20} comments</span>
+            </div>
+            <div className="Post-Footer-Likes">
+                <span>{"You & "}{300} others</span>
+                <img src={heartIcon} alt=""/>
+            </div>
+        </div>
+    </div>
+}
+ 
+export default Post;
